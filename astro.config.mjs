6d9664604f5a285912cf528e7e757e413e34d787
Alberto Ito <alberto.ito@southworks.com>
2025-07-24 02:21:37 -0300

    DeGennaro page

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['b958422c7367.ngrok-free.app'],
+    }
+  }
+});
